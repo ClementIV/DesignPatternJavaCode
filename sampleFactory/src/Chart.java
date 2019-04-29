33f816a9511f853dbfc25ffbea36822c614ff4ad
fyqnnaki@163.com <fyqnankai@163.com>
2019-04-29 16:58:12 +0800

    init

---
+++ sampleFactory/src/Chart.java
@@ -0,0 +1,6 @@
+public interface Chart {
+    /**
+     * this is a function to display chart
+     */
+    public void display();
+}
---
+++ sampleFactory/src/Client.java
@@ -0,0 +1,7 @@
+public class Client {
+    public static void main(String args[]){
+        Chart chart;
+        chart = ChartFactory.getChart("histogram");
+        chart.display();
+    }
+}
